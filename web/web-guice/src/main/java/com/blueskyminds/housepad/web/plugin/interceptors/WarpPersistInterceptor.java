c6222c88121901d30d41a9df8dd38432222a4c90
jeromyevans <jeromy.evans@blueskyminds.com.au>
2008-09-16 07:22:58 +0000

    removed hp-specific tags added warp-persist support

---
+++ web/web-guice/src/main/java/com/blueskyminds/housepad/web/plugin/interceptors/WarpPersistInterceptor.java
@@ -0,0 +1,25 @@
+package com.blueskyminds.housepad.web.plugin.interceptors;
+
+import com.opensymphony.xwork2.interceptor.Interceptor;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.wideplay.warp.persist.Transactional;
+
+/**
+ *
+ * An interceptor that starts a transaction for the view
+ *
+ * Date Started: 30/04/2008
+ */
+public class WarpPersistInterceptor implements Interceptor {
+
+    public void destroy() {
+    }
+
+    public void init() {
+    }
+
+    @Transactional
+    public String intercept(ActionInvocation invocation) throws Exception {
+        return invocation.invoke();
+    }
+}
